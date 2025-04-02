d990d0549f1104575de4e56fd8df9ad8e5775345
Charles Schiermeyer <matkatmusic@gmail.com>
2025-04-01 19:14:41 -0700

    updated DSP from params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -494,7 +494,9 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
 
     //[DONE]: add APVTS
     //[DONE]: create audio parameters for all dsp choices
-    //TODO: update DSP here from audio parameters
+    //[DONE]: update DSP here from audio parameters
+    //TODO: update general filter corrections
+    //TODO: add smoothers for all param updates
     //[DONE]: save/load settings
     //TODO: save/load DSP order
     //TODO: Drag-To-Reorder GUI
@@ -508,6 +510,26 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     //TODO: pre/post filtering [BONUS]
     //TODO: delay module [BONUS]
     
+    phaser.dsp.setRate( phaserRateHz->get() );
+    phaser.dsp.setCentreFrequency( phaserCenterFreqHz->get() );
+    phaser.dsp.setDepth( phaserDepthPercent->get() );
+    phaser.dsp.setFeedback( phaserFeedbackPercent->get() );
+    phaser.dsp.setMix( phaserMixPercent->get() );
+    
+    chorus.dsp.setRate( chorusRateHz->get() );
+    chorus.dsp.setDepth( chorusDepthPercent->get() );
+    chorus.dsp.setCentreDelay( chorusCenterDelayMs->get() );
+    chorus.dsp.setFeedback( chorusFeedbackPercent->get() );
+    chorus.dsp.setMix( chorusMixPercent->get() );
+    
+    overdrive.dsp.setDrive( overdriveSaturation->get() );
+    
+    ladderFilter.dsp.setMode( static_cast<juce::dsp::LadderFilterMode>(ladderFilterMode->getIndex()));
+    ladderFilter.dsp.setCutoffFrequencyHz( ladderFilterCutoffHz->get() );
+    ladderFilter.dsp.setResonance( ladderFilterResonance->get() );
+    ladderFilter.dsp.setDrive( ladderFilterDrive->get() );
+    
+    //temp instance to pull into
     auto newDSPOrder = DSP_Order();
     
     //try to pull
