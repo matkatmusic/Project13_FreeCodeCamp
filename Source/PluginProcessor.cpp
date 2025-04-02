776ab77b234d21adf9043e5f9e9bfd0d887743f0
Charles Schiermeyer <matkatmusic@gmail.com>
2025-04-01 17:55:47 -0700

    revised how cached float params are initialized to include all existing float params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -34,7 +34,7 @@ Project13AudioProcessor::Project13AudioProcessor()
                        )
 #endif
 {
-    auto phaserParams = std::array
+    auto floatParams = std::array
     {
         &phaserRateHz,
         &phaserCenterFreqHz,
@@ -43,7 +43,7 @@ Project13AudioProcessor::Project13AudioProcessor()
         &phaserMixPercent
     };
     
-    auto phaserFuncs = std::array
+    auto floatNameFuncs = std::array
     {
         &getPhaserRateName,
         &getPhaserCenterFreqName,
@@ -52,10 +52,10 @@ Project13AudioProcessor::Project13AudioProcessor()
         &getPhaserMixName
     };
     
-    for( size_t i = 0; i < phaserParams.size(); ++i )
+    for( size_t i = 0; i < floatParams.size(); ++i )
     {
-        auto ptrToParamPtr = phaserParams[i];
-        *ptrToParamPtr = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter( phaserFuncs[i]() ));
+        auto ptrToParamPtr = floatParams[i];
+        *ptrToParamPtr = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter( floatNameFuncs[i]() ));
         jassert( *ptrToParamPtr != nullptr );
     }
     
